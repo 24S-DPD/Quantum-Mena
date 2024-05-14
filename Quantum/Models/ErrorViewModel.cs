cdcdd84bc3d02dd16d79956ab83d1c4dca4c964f
catrobien@gmail.com <catrobien@gmail.com>
2024-05-14 15:42:23 -0700

    Project Created !!

---
+++ Quantum/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Quantum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
